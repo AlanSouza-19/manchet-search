978d2f2d013ce52eb243c678486208deb0081d32
Alan Correa <souzaalanc75@gmail.com>
2020-07-07 15:36:38 -0300

    cadastro das manchets no banco de dados

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
+import sqlite3
+from time import sleep
+# import asyncio
+
+
+def addNewManchet(a:list, b: list):
+  connection = sqlite3.connect('database.sqlite')
+  cursor = connection.cursor()
+  z = zip(a, b)
+
+  for title, link in z:
+    title = title.replace("'", "")
+    cursor.execute(f"INSERT INTO manchets (title, link) VALUES ('{title}', '{link}')")
+  
+  connection.commit()
+  connection.close()
+
 
 r = requests.get('https://g1.globo.com/')
 
@@ -8,7 +25,11 @@ html = BeautifulSoup(html_data, 'html.parser')
 
 finder = html.find_all(class_='feed-post-link gui-color-primary gui-color-hover')
 
+titles: list = []
+links: list = []
 for i in finder:
-  title = i.get_text()
-  link = i.get('href')
+  titles.append(i.get_text())
+  links.append(i.get('href'))
 
+
+addNewManchet(titles, links)
